60565b35d6ba1a85a6c33ccf8bef41dccfa900bf
Igor J.L. <nigorjeanluc@gmail.com>
2024-09-02 10:55:24 +0200

    Fix footer

--- src/components/HomePage/Welcome/index.js
+++ src/components/HomePage/Welcome/index.js
@@ -35,7 +35,7 @@ function Welcome() {
                                     <div className="welc-img" data-aos-once="true" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="600">
                                         <img className="lazyload" src={require('../../../assets/images/welcomeImg.png')} alt="Serio Group Photo" width="495" height="610" />
                                     </div>
-                                    <div className="welc-img-accent-bg">
+                                    <div style={{background: 'white'}} className="welc-img-accent-bg">
                                         <canvas width="542" height="872" className="lazyload" data-bgset="https://serioestates.com/wp-content/themes/rwserio-pending.com/images/welc-accent-logo.png"></canvas>
                                     </div>
                                 </div>
