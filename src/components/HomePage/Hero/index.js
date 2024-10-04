f16295b07b92ae98418c8f20d86e6386069b7a71
Igor J.L. Ndiramiye <nigorjeanluc@gmail.com>
2024-10-04 10:41:35 +0200

    Hero Image

--- src/components/HomePage/Hero/index.js
+++ src/components/HomePage/Hero/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import posterImage from '../../../assets/home.jpg';
 
 function Hero() {
   return (
@@ -10,8 +11,8 @@ function Hero() {
                         <canvas width="1600" height="819" className="canvas-holder"></canvas>
                         <div className="cycloneslider-slides" data-cycle-allow-wrap="true" data-cycle-dynamic-height="off" data-cycle-auto-height="1600:819" data-cycle-auto-height-easing="null" data-cycle-auto-height-speed="250" data-cycle-delay="0" data-cycle-easing="" data-cycle-fx="fade" data-cycle-hide-non-active="true" data-cycle-log="false" data-cycle-next="#cycloneslider-hp-video-1 .cycloneslider-next" data-cycle-pager="#cycloneslider-hp-video-1 .cycloneslider-pager" data-cycle-pause-on-hover="false" data-cycle-prev="#cycloneslider-hp-video-1 .cycloneslider-prev" data-cycle-slides="> div" data-cycle-speed="1000" data-cycle-swipe="1" data-cycle-tile-count="7" data-cycle-tile-delay="100" data-cycle-tile-vertical="true" data-cycle-timeout="4000">
                             <div className="cycloneslider-slide cycloneslider-slide-custom">
-                                <video id="slide-video" preload="none" poster={require('../../../assets/home.jpg')} style={{ width: '100%', height: 'auto' }} autoPlay muted playsInline loop onContextMenu={() => false} className="hp-vid">
-                                    <source src={require('../../../assets/new_5.mp4')} type="video/mp4" />
+                                <video id="slide-video" preload="none" poster={posterImage} style={{ width: '100%', height: 'auto' }} autoPlay muted playsInline loop onContextMenu={() => false} className="hp-vid">
+                                    <source src='https://d3mpuupcc30lzt.cloudfront.net/fronts/welcome.mp4' type="video/mp4" />
                                 </video>
                             </div>
                         </div>
